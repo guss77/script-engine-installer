82d549389c2c849b6a3b3836ca5e114b3ea8d4db
Oded Arbel <oded@geek.co.il>
2015-12-15 14:28:47 +0200

    allow to override RVM installer download with another URL

--- install-script-engine.sh
+++ install-script-engine.sh
@@ -25,6 +25,14 @@ function die() {
   exit 1
 }
 
+function rvm_source_url() {
+  if [ -n "$RVM_URL" ]; then
+    echo "$RVM_URL"
+  else
+    echo "https://get.rvm.io"
+  fi
+}
+
 function rvm_base_dir() {
   if [ -n "$INSTALL_CWD" ]; then
     echo "$(pwd)/.rvm"
@@ -77,7 +85,7 @@ function install_ruby() {
   if [ -d "$(rvm_base_dir)" ]; then
     $(rvm_base_dir)/bin/rvm-shell -c 'rvm get stable'
   else
-    $CURL -sL https://get.rvm.io | bash -s -- --path "$(rvm_base_dir)" stable
+    $CURL -sL $(rvm_source_url) | bash -s -- --path "$(rvm_base_dir)" stable
     all_ok "${PIPESTATUS[@]}" || die "Failed to install RVM"
   fi | un_ansi
   if [ -n "$INSTALL_CWD" ]; then
